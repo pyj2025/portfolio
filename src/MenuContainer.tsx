2dc60eaa21374ec9aecd944794c4f5d69a627349
Youngjoon Park <pyj2025@gmail.com>
2021-10-11 21:58:07 -0500

    add menu button style

--- src/MenuContainer.tsx
+++ src/MenuContainer.tsx
@@ -7,43 +7,47 @@ import {
   faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 
 const SidebarContainer = styled.div`
-  height: 100%;
-  width: 100%;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
   background-color: #333333;
   color: white;
-  /* display: flex;
-  align-items: center;
-  justify-content: center;
+`;
+
+const MenuWrapper = styled.div`
+  display: flex;
   position: relative;
   flex-wrap: wrap;
   flex-shrink: 1;
+  justify-content: center;
+  align-items: center;
   overflow-y: auto;
-  padding: 1rem; */
 `;
 
 const ListItem = styled.a`
-  /* font-size: 1.5em;
-  text-align: center;
-  transition: background-color 0.2s;
   display: flex;
-  align-items: center;
-  box-sizing: border-box;
-  width: 8rem;
-  height: 8rem;
   flex-direction: column;
   justify-content: center;
-  padding: 1rem;
-  border-radius: 0.2rem; */
-
+  align-items: center;
+  text-align: center;
+  font-size: 1.5em;
+  color: white;
   width: 8rem;
   height: 8rem;
-  color: white;
+  margin: 0 auto;
+  box-sizing: border-box;
+  transition: background-color 0.2s;
+  border-radius: 0.2rem;
+  padding: 1rem;
+
   text-decoration: none;
   cursor: pointer;
+
   :hover {
     background-color: #3c3c3c;
   }
@@ -62,34 +66,34 @@ const MenuContainer: React.FC<MenuContainerProps> = ({
   toggleSkillsOpen,
   toggleProjectsOpen,
 }) => {
-  const [isOpen, setOpen] = useState(false);
-
   return (
     <SidebarContainer>
-      <ListItem onClick={toggleAboutOpen}>
-        <FontAwesomeIcon icon={faUser} />
-        About
-      </ListItem>
-      <ListItem onClick={toggleSkillsOpen}>
-        <FontAwesomeIcon icon={faCode} />
-        Skills
-      </ListItem>
-      <ListItem onClick={toggleProjectsOpen}>
-        <FontAwesomeIcon icon={isProjectsOpen ? faFolderOpen : faFolder} />
-        Projects
-      </ListItem>
-      <ListItem onClick={toggleProjectsOpen}>
-        <FontAwesomeIcon icon={faFile} />
-        Resume
-      </ListItem>
-      <ListItem href="https://github.com/pyj2025">
-        <FontAwesomeIcon icon={faGithub} />
-        Github
-      </ListItem>
-      <ListItem href="https://www.linkedin.com/in/devjoon/">
-        <FontAwesomeIcon icon={faLinkedin} />
-        Linkedin
-      </ListItem>
+      <MenuWrapper>
+        <ListItem onClick={toggleAboutOpen}>
+          <FontAwesomeIcon icon={faUser} />
+          About
+        </ListItem>
+        <ListItem onClick={toggleSkillsOpen}>
+          <FontAwesomeIcon icon={faCode} />
+          Skills
+        </ListItem>
+        <ListItem onClick={toggleProjectsOpen}>
+          <FontAwesomeIcon icon={isProjectsOpen ? faFolderOpen : faFolder} />
+          Projects
+        </ListItem>
+        <ListItem onClick={toggleProjectsOpen}>
+          <FontAwesomeIcon icon={faFile} />
+          Resume
+        </ListItem>
+        <ListItem href="https://github.com/pyj2025">
+          <FontAwesomeIcon icon={faGithub} />
+          Github
+        </ListItem>
+        <ListItem href="https://www.linkedin.com/in/devjoon/">
+          <FontAwesomeIcon icon={faLinkedin} />
+          Linkedin
+        </ListItem>
+      </MenuWrapper>
     </SidebarContainer>
   );
 };
