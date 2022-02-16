3eeb8c41e7391e604e39f78d721e0ebf2619c8b6
Youngjoon Park <pyj2025@gmail.com>
2022-02-15 20:57:46 -0600

    redesign about window body

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,41 +2,43 @@ import React from "react";
 import styled from "styled-components";
 import { BoldText } from "../GlobalStyle";
 
-const AboutContainer = styled.div`
-  display: grid;
-  grid-template-rows: 1fr 1fr 1fr 1fr;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
   justify-content: center;
   margin: 10px;
 `;
 
-const DataRowContainer = styled.div`
+const DataRow = styled.div`
   display: grid;
   grid-template-columns: 120px auto;
+  margin-bottom: 4px;
 `;
 
 const About: React.FC = () => {
   return (
-    <AboutContainer>
-      <DataRowContainer>
+    <Container>
+      <DataRow>
         <BoldText>Name</BoldText>
         <div>Youngjoon Park</div>
-      </DataRowContainer>
-      <DataRowContainer>
+      </DataRow>
+      <DataRow>
         <BoldText>Date of Birth</BoldText>
         <div>Jan.17.1994</div>
-      </DataRowContainer>
-      <DataRowContainer>
+      </DataRow>
+      <DataRow>
         <BoldText>Phone</BoldText>
         <div>+1 312-937-4435</div>
-      </DataRowContainer>
-      <DataRowContainer>
+      </DataRow>
+      <DataRow>
         <BoldText>Address</BoldText>
         <div>
           <div>25 W Randolph St Apt 903,</div>
           <div>Chicago, IL, 60601</div>
         </div>
-      </DataRowContainer>
-    </AboutContainer>
+      </DataRow>
+    </Container>
   );
 };
 
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -2,37 +2,42 @@ import React from "react";
 import styled from "styled-components";
 import { BoldText } from "../GlobalStyle";
 
-const ExperienceContainer = styled.div`
-  display: grid;
-  grid-template-rows: 1fr 1fr 1fr 1fr;
-  grid-gap: 5px;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
   margin: 10px;
+`;
+
+const DataRow = styled.div`
+  display: grid;
+  margin-bottom: 4px;
 `;
 
 const Experience: React.FC = () => {
   return (
-    <ExperienceContainer>
-      <div>
+    <Container>
+      <DataRow>
         <BoldText>Junior Frontend Developer</BoldText>
         <div>Enfusion, Chicago, IL, USA</div>
         <div>Jan 2020 - Present</div>
-      </div>
-      <div>
+      </DataRow>
+      <DataRow>
         <BoldText>Undergraduate Teaching Assistant</BoldText>
         <div>Purdue University, West Lafayette, IN, USA</div>
         <div>Aug 2018 - Dec 2019</div>
-      </div>
-      <div>
+      </DataRow>
+      <DataRow>
         <BoldText>Technical Consultant Intern</BoldText>
         <div>Dotis, Seoul, South Korea</div>
         <div>Jun 2017 - Jul 2017</div>
-      </div>
-      <div>
+      </DataRow>
+      <DataRow>
         <BoldText>Software Engineering Intern</BoldText>
         <div>Hyop Woon, Seoul, South Korea</div>
         <div>Jul 2015 - Aug 2015</div>
-      </div>
-    </ExperienceContainer>
+      </DataRow>
+    </Container>
   );
 };
 
