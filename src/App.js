42c1b46a6b85efc892dfd15f2e23ab93affdd589
Youngjoon Park <pyj2025@gmail.com>
2025-07-02 23:37:12 -0700

    add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 import { isIE } from "react-device-detect";
 import Maintenance from "./components/Maintenance";
 import NotSupport from "./components/NotSupport";
@@ -8,13 +6,7 @@ import MainApp from "./MainApp";
 const inMaintenance = false;
 
 function App() {
-  const component = inMaintenance ? (
-    <Maintenance />
-  ) : isIE ? (
-    <NotSupport />
-  ) : (
-    <MainApp />
-  );
+  const component = inMaintenance ? <Maintenance /> : isIE ? <NotSupport /> : <MainApp />;
 
   return <>{component}</>;
 }
