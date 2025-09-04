f750be7be4799178eec639de94537fd5d1f8b2d8
Youngjoon Park <pyj2025@gmail.com>
2025-09-03 23:26:08 -0700

    update Education

--- src/components/about/Education.tsx
+++ src/components/about/Education.tsx
@@ -1,63 +1,67 @@
-import React from 'react';
-import styled from 'styled-components';
-import {
-  MutedText,
-  Panel,
-  PanelContainer,
-  PanelTableContainer,
-  PanelTableLabel,
-} from '../../GlobalStyle';
-import PurdueLogo from '../../image/PurdueLogo.png';
-import info from '../../info.json';
+import React from "react";
+import PurdueLogo from "../../image/PurdueLogo.png";
+import info from "../../info.json";
 
-const PurdueLogoImage = styled.img`
-  width: 150px;
-  height: 150px;
-`;
+const LabelContainerStyle = "flex w-32";
+const LabelTextStyle = "text-gray-500";
+const ValueTextStyle = "break-words";
 
 const Education: React.FC = () => {
   return (
-    <Panel>
-      <PanelContainer>
-        <PurdueLogoImage src={PurdueLogo} alt="Purdue" />
-      </PanelContainer>
-      <PanelContainer>
-        <PanelTableContainer>
-          <tr>
-            <PanelTableLabel>
-              <MutedText>Name</MutedText>
-            </PanelTableLabel>
-            <td>{info.about.education.university.name}</td>
-          </tr>
-          <tr>
-            <PanelTableLabel>
-              <MutedText>Graduated</MutedText>
-            </PanelTableLabel>
-            <td>
-              {`${info.about.education.university.graduateYear.month} ${info.about.education.university.graduateYear.year}`}
-            </td>
-          </tr>
-          <tr>
-            <PanelTableLabel>
-              <MutedText>Degree</MutedText>
-            </PanelTableLabel>
-            <td>{info.about.education.university.degree}</td>
-          </tr>
-          <tr>
-            <PanelTableLabel>
-              <MutedText>Major</MutedText>
-            </PanelTableLabel>
-            <td>{info.about.education.university.major}</td>
-          </tr>
-          <tr>
-            <PanelTableLabel>
-              <MutedText>Concentration</MutedText>
-            </PanelTableLabel>
-            <td>{info.about.education.university.concentration}</td>
-          </tr>
-        </PanelTableContainer>
-      </PanelContainer>
-    </Panel>
+    <div className="flex flex-col flex-wrap justify-start min-w-80 m-2.5">
+      <div className="flex justify-start items-center m-2 px-20">
+        <div className="relative w-[150px] h-[150px] overflow-hidden">
+          <img src={PurdueLogo} alt="Purdue" className="w-full h-full object-contain" />
+        </div>
+      </div>
+
+      <div className="flex justify-start items-center m-2 px-4">
+        <table className="border-spacing-1 w-full table-fixed">
+          <colgroup>
+            <col className="w-32" />
+            <col />
+          </colgroup>
+          <tbody>
+            <tr>
+              <td className={LabelContainerStyle}>
+                <span className={LabelTextStyle}>Name</span>
+              </td>
+              <td className={ValueTextStyle}>{info.about.education.university.name}</td>
+            </tr>
+
+            <tr>
+              <td className={LabelContainerStyle}>
+                <span className={LabelTextStyle}>Graduated</span>
+              </td>
+              <td className={ValueTextStyle}>
+                {`${info.about.education.university.graduateYear.month} ${info.about.education.university.graduateYear.year}`}
+              </td>
+            </tr>
+
+            <tr>
+              <td className={LabelContainerStyle}>
+                <span className={LabelTextStyle}>Degree</span>
+              </td>
+              <td className={ValueTextStyle}>{info.about.education.university.degree}</td>
+            </tr>
+
+            <tr>
+              <td className={LabelContainerStyle}>
+                <span className={LabelTextStyle}>Major</span>
+              </td>
+              <td className={ValueTextStyle}>{info.about.education.university.major}</td>
+            </tr>
+
+            <tr>
+              <td className={LabelContainerStyle}>
+                <span className={LabelTextStyle}>Concentration</span>
+              </td>
+              <td className={ValueTextStyle}>{info.about.education.university.concentration}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 };
 
