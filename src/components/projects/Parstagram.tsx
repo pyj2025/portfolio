c79e0ff693997fec4c0613619e9c69cd4c910eee
Youngjoon Park <pyj2025@gmail.com>
2025-09-09 22:28:55 -0700

    update Project Details

--- src/components/projects/Parstagram.tsx
+++ src/components/projects/Parstagram.tsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import info from '../../info.json';
-import ParstagramLogo from '../../image/projects/Parstagram.png';
-import { Panel, PanelLogoImage, PanelContainer } from '../../GlobalStyle';
-import ProjectDescriptionTable from './ProjectDescriptionTable';
+import React from "react";
+import info from "../../info.json";
+import ParstagramLogo from "../../image/projects/Parstagram.png";
+import ProjectDescriptionTable from "./ProjectDescriptionTable";
 
 const Parstagram: React.FC = () => {
   return (
-    <Panel>
-      <PanelContainer>
-        <PanelLogoImage src={ParstagramLogo} alt="Parstagram" />
-      </PanelContainer>
+    <div className="flex flex-col flex-wrap justify-center min-w-80 m-2.5">
+      <div className="flex justify-center items-center m-2">
+        <img src={ParstagramLogo} alt="Parstagram" className="w-36 h-36" />
+      </div>
       <ProjectDescriptionTable
         name={info.project.Parstagram.name}
         link={info.project.Parstagram.link}
         stack={info.project.Parstagram.stack}
         details={info.project.Parstagram.details}
       />
-    </Panel>
+    </div>
   );
 };
 
