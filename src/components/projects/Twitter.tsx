02fa7588ee314a9ae043bb963c08ea3a7516a9fb
Youngjoon Park <pyj2025@gmail.com>
2025-09-10 23:16:31 -0700

    update Project Details

--- src/components/projects/Twitter.tsx
+++ src/components/projects/Twitter.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import info from '../../info.json';
-import { Panel, PanelLogoImage, PanelContainer } from '../../GlobalStyle';
-import TwitterLogo from '../../image/projects/Twitter.png';
-import ProjectDescriptionTable from './ProjectDescriptionTable';
+import React from "react";
+import info from "../../info.json";
+import TwitterLogo from "../../image/projects/Twitter.png";
+import ProjectDescriptionTable from "./ProjectDescriptionTable";
 
 const Twitter: React.FC = () => {
   return (
-    <Panel>
-      <PanelContainer>
-        <PanelLogoImage src={TwitterLogo} alt="Twitter" />
-      </PanelContainer>
-      <PanelContainer>
-        <ProjectDescriptionTable
-          name={info.project.Twitter.name}
-          link={info.project.Twitter.link}
-          stack={info.project.Twitter.stack}
-          details={info.project.Twitter.details}
-        />
-      </PanelContainer>
-    </Panel>
+    <div className="flex flex-col flex-wrap justify-center min-w-80 m-2.5">
+      <div className="flex justify-center items-center m-2">
+        <img src={TwitterLogo} alt="Twitter" className="w-36 h-36" />
+      </div>
+      <ProjectDescriptionTable
+        name={info.project.Twitter.name}
+        link={info.project.Twitter.link}
+        stack={info.project.Twitter.stack}
+        details={info.project.Twitter.details}
+      />
+    </div>
   );
 };
 
--- src/components/projects/WebGame.tsx
+++ src/components/projects/WebGame.tsx
@@ -1,23 +1,19 @@
-import React from 'react';
-import info from '../../info.json';
-
-import { Panel, PanelContainer } from '../../GlobalStyle';
-import { getIcon } from '../getIcon';
-import ProjectDescriptionTable from './ProjectDescriptionTable';
+import React from "react";
+import info from "../../info.json";
+import { getIcon } from "../getIcon";
+import ProjectDescriptionTable from "./ProjectDescriptionTable";
 
 const WebGame: React.FC = () => {
   return (
-    <Panel>
-      <PanelContainer>{getIcon('CodeFile')}</PanelContainer>
-      <PanelContainer>
-        <ProjectDescriptionTable
-          name={info.project.WebGame.name}
-          link={info.project.WebGame.link}
-          stack={info.project.WebGame.stack}
-          details={info.project.WebGame.details}
-        />
-      </PanelContainer>
-    </Panel>
+    <div className="flex flex-col flex-wrap justify-center min-w-80 m-2.5">
+      <div className="flex justify-center items-center m-2">{getIcon("CodeFile")}</div>
+      <ProjectDescriptionTable
+        name={info.project.WebGame.name}
+        link={info.project.WebGame.link}
+        stack={info.project.WebGame.stack}
+        details={info.project.WebGame.details}
+      />
+    </div>
   );
 };
 
