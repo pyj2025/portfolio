17d0bfa552fef91beff9e85c446dbffe1b663fec
Youngjoon Park <pyj2025@gmail.com>
2025-09-16 19:52:27 -0700

    update Menu

--- src/components/dock/Menu.tsx
+++ src/components/dock/Menu.tsx
@@ -1,66 +1,22 @@
-import { faCircle } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
-import styled from 'styled-components';
-import { IconProp } from '@fortawesome/fontawesome-svg-core';
-import { getIcon } from '../getIcon';
-import useAboutStore from '../../utils/useAboutStore';
-import useSkillsStore from '../../utils/useSkillsStore';
-import useProjectsStore from '../../utils/useProjectsStore';
+import React from "react";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { getIcon } from "../getIcon";
+import useAboutStore from "../../utils/useAboutStore";
+import useSkillsStore from "../../utils/useSkillsStore";
+import useProjectsStore from "../../utils/useProjectsStore";
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: flex-end;
-  width: 100%;
-  height: 100%;
-`;
+const WindowMenuItemStyle =
+  "relative flex flex-col justify-center items-center text-center text-white mx-auto box-border transition-colors duration-200 rounded-sm p-4 cursor-pointer hover:bg-yellow-50/10";
 
-const MenuWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  border-radius: 6px;
-  box-shadow: 2px 7px 15px 8px rgba(0, 0, 0, 0.4);
-  background-color: rgba(255, 255, 192, 0.1);
-  backdrop-filter: blur(10px);
-`;
-
-const MenuItem = styled.a`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  color: white;
-  margin: 0 auto;
-  box-sizing: border-box;
-  transition: background-color 0.2s;
-  border-radius: 0.2rem;
-  padding: 1rem;
-  text-decoration: none;
-  cursor: pointer;
-
-  :hover {
-    background-color: rgba(255, 255, 192, 0.1);
-  }
-`;
-
-const MinimizedIcon = styled(FontAwesomeIcon)`
-  position: absolute;
-  height: 4px;
-  width: 4px;
-  padding-top: 3.5rem;
-  color: #aaaaaa;
-`;
+const ExternalLinkMenuItemStyle =
+  "flex flex-col justify-center items-center text-center text-white mx-auto box-border transition-colors duration-200 rounded-sm p-4 no-underline cursor-pointer hover:bg-yellow-50/10";
 
 const Menu: React.FC = () => {
-  const { isAboutMinimized, openAbout } = useAboutStore((state) => state);
-  const { isSkillsMinimized, openSkills } = useSkillsStore((state) => state);
-  const { isProjectsMinimized, openProjects } = useProjectsStore(
-    (state) => state
-  );
+  const { isAboutMinimized, openAbout } = useAboutStore(state => state);
+  const { isSkillsMinimized, openSkills } = useSkillsStore(state => state);
+  const { isProjectsMinimized, openProjects } = useProjectsStore(state => state);
 
   const handleAboutClick = React.useCallback(() => {
     openAbout();
@@ -75,54 +31,80 @@ const Menu: React.FC = () => {
   }, [openProjects]);
 
   const handleEmailClick = React.useCallback(() => {
-    window.open('mailto:pyj2025@gmail.com');
+    window.open("mailto:pyj2025@gmail.com");
   }, []);
 
   return (
-    <Container>
-      <MenuWrapper>
-        <MenuItem title="About" onClick={handleAboutClick}>
-          {getIcon('About')}
+    <div className="flex justify-center items-end w-full h-full">
+      <div className="flex flex-row justify-center items-center rounded-md shadow-2xl bg-yellow-50/10 backdrop-blur-md">
+        <button title="About" onClick={handleAboutClick} className={WindowMenuItemStyle}>
+          {getIcon("About")}
           {isAboutMinimized ? (
-            <MinimizedIcon icon={faCircle as IconProp} />
+            <FontAwesomeIcon
+              icon={faCircle as IconProp}
+              className="absolute h-1 w-1 pt-14 text-gray-400"
+            />
           ) : null}
-        </MenuItem>
-        <MenuItem title="Skills" onClick={handleSkillsClick}>
-          {getIcon('Skills')}
+        </button>
+        <button title="Skills" onClick={handleSkillsClick} className={WindowMenuItemStyle}>
+          {getIcon("Skills")}
           {isSkillsMinimized ? (
-            <MinimizedIcon icon={faCircle as IconProp} />
+            <FontAwesomeIcon
+              icon={faCircle as IconProp}
+              className="absolute h-1 w-1 pt-14 text-gray-400"
+            />
           ) : null}
-        </MenuItem>
-        <MenuItem title="Projects" onClick={handleProjectsClick}>
-          {getIcon('Projects')}
+        </button>
+        <button title="Projects" onClick={handleProjectsClick} className={WindowMenuItemStyle}>
+          {getIcon("Projects")}
           {isProjectsMinimized ? (
-            <MinimizedIcon icon={faCircle as IconProp} />
+            <FontAwesomeIcon
+              icon={faCircle as IconProp}
+              className="absolute h-1 w-1 pt-14 text-gray-400"
+            />
           ) : null}
-        </MenuItem>
-        <MenuItem
+        </button>
+        <a
           title="Resume"
           href="https://drive.google.com/file/d/14bb5ogfmAumTw7cMA_0PEUVwsPE_mOd-/view?usp=sharing"
           target="_blank"
+          rel="noopener noreferrer"
+          className={ExternalLinkMenuItemStyle}
         >
-          {getIcon('Resume')}
-        </MenuItem>
-        <MenuItem title="Github" href="https://github.com/pyj2025">
-          {getIcon('Github')}
-        </MenuItem>
-        <MenuItem title="Linkedin" href="https://www.linkedin.com/in/devjoon/">
-          {getIcon('Linkedin')}
-        </MenuItem>
-        <MenuItem
+          {getIcon("Resume")}
+        </a>
+        <a
+          title="Github"
+          href="https://github.com/pyj2025"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={ExternalLinkMenuItemStyle}
+        >
+          {getIcon("Github")}
+        </a>
+        <a
+          title="Linkedin"
+          href="https://www.linkedin.com/in/devjoon/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={ExternalLinkMenuItemStyle}
+        >
+          {getIcon("Linkedin")}
+        </a>
+        <a
           title="Facebook"
           href="https://www.facebook.com/youngjoon.park.71"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={ExternalLinkMenuItemStyle}
         >
-          {getIcon('Facebook')}
-        </MenuItem>
-        <MenuItem title="Email" onClick={handleEmailClick}>
-          {getIcon('Email')}
-        </MenuItem>
-      </MenuWrapper>
-    </Container>
+          {getIcon("Facebook")}
+        </a>
+        <button title="Email" onClick={handleEmailClick} className={ExternalLinkMenuItemStyle}>
+          {getIcon("Email")}
+        </button>
+      </div>
+    </div>
   );
 };
 
