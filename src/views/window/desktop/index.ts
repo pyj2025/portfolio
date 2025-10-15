15ba455edec4f4f2facf0ee8a87ea4d738adfa2e
Youngjoon Park <pyj2025@gmail.com>
2025-10-14 22:25:15 -0700

    update BodyContent and desk comp

---
+++ src/views/window/desktop/index.ts
@@ -0,0 +1,4 @@
+export { default as WelcomeWindow } from "./WelcomeWindow";
+export { default as AboutWindow } from "./AboutWindow";
+export { default as SkillsWindow } from "./SkillsWindow";
+export { default as ProjectsWindow } from "./ProjectsWindow";
