33217d947443e38b9e29a1a08b12911ca26f30a5
Youngjoon Park <pyj2025@gmail.com>
2026-08-04 21:02:09 -0700

    refactor todos

--- src/components/todo/constants.ts
+++ src/components/todo/constants.ts
@@ -1,9 +1,6 @@
-export type Todo = {
-  id: string;
-  text: string;
-};
+import { TodoType } from "./types";
 
-export const TODOS: Todo[] = [
+export const TODOS: TodoType[] = [
   { id: "aws-ai-pro", text: "Study for AWS AI Practitioner exam" },
   { id: "aws-saa", text: "Study for AWS Solutions Architect Professional exam" },
   { id: "aws-dev", text: "Study for AWS Developer Professional exam" },
